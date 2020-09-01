60d0ad8f7660748b9a2a60291d089a821bdfc0ca
DevJewellers <66473483+DevJewellers@users.noreply.github.com>
2020-09-01 18:37:41 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('arnav5544').then(function(cache) {
+     return cache.addAll([
+       'https://adevjewellers.github.io',
+       'index.html',
+       'script.js',
+       'style.css',
+       'devjewellers512.png',
+'devjewellers.webmanifest',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
